b2d3e8533fc35658b483c13066aa2e9e69aded11
timur <timurchic94@gmail.com>
2023-12-12 21:57:23 +0700

    commit

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO user (firstname, secondname, age) VALUES ('Иван', 'Иванович', '40');
-INSERT INTO user (firstname, secondname, age) VALUES ('Андрей', 'Андреевич', '20');
-INSERT INTO user (firstname, secondname, age) VALUES ('Сергей', 'Сергеевич', '35');
-INSERT INTO user (firstname, secondname, age) VALUES ('Анатолий', 'Анатольевич', '54');
+INSERT INTO users (firstname, secondname, age) VALUES ('Иван', 'Иванович', '40');
+INSERT INTO users (firstname, secondname, age) VALUES ('Андрей', 'Андреевич', '20');
+INSERT INTO users (firstname, secondname, age) VALUES ('Сергей', 'Сергеевич', '35');
+INSERT INTO users (firstname, secondname, age) VALUES ('Анатолий', 'Анатольевич', '54');
